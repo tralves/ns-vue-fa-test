6d1edb14c48c851a56bffcee7aa101a5c2f3d4a1
Tiago Alves <tralves@gmail.com>
2018-11-13 21:14:59 +0000

    first commit

---
+++ app/components/App.vue
@@ -0,0 +1,34 @@
+<template>
+    <Page>
+        <ActionBar title="Welcome to NativeScript-Vue!"/>
+        <StackLayout columns="*" rows="*">
+            <Label class="message" :text="msg" col="0" row="0"/>
+            <Label class="fa" :text="'fa-map-o' | fonticon" />
+
+        </StackLayout>
+    </Page>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: 'Hello World!',
+    }
+  },
+}
+</script>
+
+<style scoped>
+ActionBar {
+  background-color: #53ba82;
+  color: #ffffff;
+}
+
+.message {
+  vertical-align: center;
+  text-align: center;
+  font-size: 20;
+  color: #333333;
+}
+</style>
